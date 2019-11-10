5ae52714a1b293c4246f4e33816fb7c14bee14c7
helibertoarias <HelibertoArias@gmail.com>
2019-11-10 10:49:10 -0500

    feat: add user-setting-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { UserSettingsFormsComponent } from './user-settings-forms/user-settings-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
